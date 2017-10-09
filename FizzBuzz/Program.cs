d40ddf6b7b8966b028fb30011a6f022a3152a066
zabeen <zabeen.patel@ucl.ac.uk>
2017-10-09 12:11:31 +0100

    Added fizzbang function

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,30 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 101; i++)
+            {
+                // default output msg is val of i
+                string msg = i.ToString();
+
+                // i is divisible by 3 and 5
+                if (i%15 == 0)
+                {
+                    msg = "FizzBang";
+                }
+                // i is divisible by 3
+                else if (i%3 == 0)
+                {
+                    msg = "Fizz";
+                }
+                // i is divisible by 5
+                else if (i%5 == 0)
+                {
+                    msg = "Bang";
+                }
+
+                Console.WriteLine(msg);
+            }
+
         }
     }
 }
